32480e545d1407b2b4b5632be7474974daceb06c
Leonel Fiomelo <leonelfiomelo@hotmail.com>
2022-09-20 09:33:42 -0300

    First commit

---
+++ app/src/main/java/com/example/app_phonoaudiology/ConfiguracionActivity.java
@@ -0,0 +1,47 @@
+package com.example.app_phonoaudiology;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ConfiguracionActivity extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public ConfiguracionActivity() {
+        // Required empty public constructor
+    }
+
+    public static ConfiguracionActivity newInstance(String param1, String param2) {
+        ConfiguracionActivity fragment = new ConfiguracionActivity();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_configuracion, container, false);
+    }
+}
